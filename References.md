fa2a66b73df495b83849edf745d3aa35fd316f1b
Daniel P. Clark <6ftdan@gmail.com>
2014-10-18 01:18:18 -0400

    Template for Resque worker.  Started notes doc for referencing used material.

---
+++ References.md
@@ -0,0 +1,10 @@
+# Details on project resources used in no particular order
+
+
+
+
+###Resque
+ - A variation on a Heroku worker autoscaler from http://blog.leshill.org/blog/2011/04/03/using-resque-and-resque-scheduler-on-heroku.html
+ 
+###RedisCloud
+
---
+++ app/workers/youtube_getter.rb
@@ -0,0 +1,15 @@
+class YouTubeGetter
+  extend HerokuResqueAutoScale::AutoScaling if Rails.env == "production"
+  @queue = :youtube_getter_queue
+
+  def defer(*args)
+    Resque.enqueue(YoouTubeGetter, self.id, *args)
+  end
+
+  def self.perform(game_id, *args)
+    # Get game name from Game Object and
+    # get a list results of videos for
+    # game.  Create maybe 10 Video objects
+    # in DB with Game as parent.
+  end
+end
