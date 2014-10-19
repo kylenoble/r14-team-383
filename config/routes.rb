16a8c8a929cac49a87cb88fac3f82bd8961e8e96
Daniel P. Clark <6ftdan@gmail.com>
2014-10-19 19:43:21 -0400

    changed game-view path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :games, only: :index
   match '/games/show/', to: 'games#show', via: [:get, :patch], as: 'show_game'
-  match '/view_game/', to: 'games#view', via: [:get, :patch], as: 'view_game'
+  match '/game/view/', to: 'games#view', via: [:get, :patch], as: 'view_game'
   mount Resque::Server, :at => "/admin/resque"
 
 end
