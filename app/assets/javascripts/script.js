01f5afb144f247e22c2f913bf4490f6d40d92077
Brett Smith <jbsmith731@gmail.com>
2014-10-19 18:33:45 -0400

    CSS edits

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,18 +1,29 @@
 $( window ).scroll(function() {
  if ($(this).scrollTop() > 400) {
       $('.navbar-inverse').addClass('fill').removeClass('nofill');
-      $('.navbar-inverse form').show();
+      $('.navbar-inverse form').fadeIn('slow');
  } else {
       $('.navbar-inverse').removeClass('fill').addClass('nofill');
-      $('.navbar-inverse form').hide();
+      $('.navbar-inverse form').fadeOut('slow');
   }
 });
 
 
-if ($('nav').hasClass('fill')) {
-	$('.navbar-inverse form').show();
-}
-
 $(window).ready(function() {
 	$("th").removeClass("tooltip");
-})
+});
+
+$(document).ready(function() {
+	$('#search').keyup(function() {
+		$('.panel').removeClass('hidden');
+	});
+});
+
+// $(document).ready(function() {
+	
+// 	if ($('#search').val().length > 0 {
+// 		$('.panel').show();
+// 	});
+	
+
+// });
