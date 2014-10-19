6d654560b4f7081fdfa7dbac0c41633019a53fc5
Daniel P. Clark <6ftdan@gmail.com>
2014-10-19 19:20:08 -0400

    fix redis

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,19 @@
 class GamesController < ApplicationController
-
-
   # GET /games
   # GET /games.json
   def index
     search_term = params[:query]
   end
 
-  def show
-    @game = Game.first
-    respond_to do |format|
-      format.js {render 'sample'} # Change render when ready
-      format.html {render 'show', object: :game, layout: nil} # Change render when ready
-    end
-  end
-
   def view
-    @game = Game.first
+    @game = Game.where(id: params["id"]).first
     @video = Video.where(game_id: @game.id).sort_by_quality.first
     @links = Link.all.take(8).shuffle
-    render layout: nil
-  end
+    respond_to do |format|
+      format.js {render layout: nil}
+      format.html {render layout: nil}
+    end
 
-  private
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def game_params
-    params.require("game").permit(:id)
   end
 
 end
